c1a2714e7bfab154b862244dcfae1f8db88149e8
Apollo Vieira <sv.apollo11@gmail.com>
2021-09-12 12:36:19 -0300

    abertura

---
+++ src/main/java/com/svapollo/springboot/SpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.svapollo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringbootApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
